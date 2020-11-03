c047a6ccb2fa51337876da70272c94f009d814db
blood <blood@example.com>
2020-11-03 13:43:57 +0800

    创建并配置APP功能模块

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'article',  # 注册 article 应用
 ]
 
 MIDDLEWARE = [
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # 配置 article 应用的分发路由
+    path('article/', include('article.urls', namespace='article')),
 ]
