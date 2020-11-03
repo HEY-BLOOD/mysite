1ae226458977892bfed14c06463b2c1f5c7e96e2
blood <blood@example.com>
2020-11-03 15:28:09 +0800

    编写文章详情页面，实现网页切换

--- article/urls.py
+++ article/urls.py
@@ -7,4 +7,6 @@ app_name = 'article'
 urlpatterns = [
     # path函数将url映射到视图
     path('article-list/', views.article_list, name='article_list'),
+    # 文章详情，Django2.0的 path新语法用尖括号<>定义需要传递的参数。
+    path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
 ]
