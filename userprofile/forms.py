ac3a8069864189ed9e0d5ac9ff384282d0ddf497
blood <blood@example.com>
2020-11-03 18:22:31 +0800

    用户的注册

--- userprofile/forms.py
+++ userprofile/forms.py
@@ -6,3 +6,23 @@ class UserLoginForm(forms.Form):
     """ 登录表单，继承自 forms.Form 类 """
     username = forms.CharField()
     password = forms.CharField()
+
+
+class UserRegisterForm(forms.ModelForm):
+    """ 注册用户表单 """
+    # 复写 User 的密码
+    password = forms.CharField()
+    password2 = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+    # def clean_[字段]这种写法Django会自动调用，来对单个字段的数据进行验证清洗。
+    def clean_password2(self):
+        """ 对两次输入的密码是否一致进行检查 """
+        data = self.cleaned_data
+        if data.get('password') == data.get('password2'):
+            return data.get('password')
+        else:
+            raise forms.ValidationError("密码输入不一致,请重试。")
