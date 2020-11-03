650c687116d97b5eb80b9f7725137d7c4ca8e23b
blood <blood@example.com>
2020-11-03 22:40:43 +0800

    文章分页

--- article/views.py
+++ article/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+# 引入分页模块
+from django.core.paginator import Paginator
 from .models import ArticlePost
 from .forms import ArticlePostForm
 import markdown
@@ -10,8 +12,16 @@ import markdown
 # Create your views here.
 def article_list(request):
     """ 文章列表 """
-    # 取出所有博客文章
-    articles = ArticlePost.objects.all()
+    # 取出所有博客文章，修改变量名称（articles -> article_list）
+    article_list = ArticlePost.objects.all()
+
+    # 每页显示 1 篇文章
+    paginator = Paginator(article_list, 2)
+    # 获取 url 中的页码
+    page = request.GET.get('page')
+    # 将导航对象相应的页码内容返回给 articles
+    articles = paginator.get_page(page)
+
     # 需要传递给模板（templates）的对象
     context = {'articles': articles}
     # render函数：载入模板，并返回context对象
