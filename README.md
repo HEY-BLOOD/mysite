e7024fe79ef9e5b889abcd81741df47fe5876f41
blood <blood@example.com>
2020-11-27 21:12:18 +0800

    update README, and scripts.fake

--- README.md
+++ README.md
@@ -1,54 +1,56 @@
 # mysite
 
-A blog application developed using the Django framework.
+使用Django框架开发的博客系统，从 [杜赛的博客](https://www.dusaiphoto.com/) 学来的。
 
-Based on the Python 3.7、Django 2.2、Bootstrap 4
+基于 Python 3.7、Django 2.2、Bootstrap 4
 
-Learn [Django搭建个人博客](https://www.dusaiphoto.com/article/2/) from [杜赛的博客](https://www.dusaiphoto.com/)
+## 安装
 
-## Installation
-
-clone:
+**克隆仓库：**
 
 ```
 $ git clone https://github.com/HEY-BLOOD/mysite.git
 $ cd mysite
 ```
 
-create & active virtual environment:
+**使用虚拟环境：**
 
 ```
 $ python -m venv env  # use `virtualenv env` for Python2, use `python3 ...` for Python3 on Linux & macOS
 $ source env/bin/activate  # use `env\Scripts\activate` on Windows
 ```
 
-install dependencies:
+**安装项目依赖：**
 
 ```
 $ pip install -r requirements.txt
 ```
 
-migrate database:
+**迁移数据库：**
 
 ```
 $ python manage.py migrate
 ```
 
-create virtual data:
-
-In addition to some virtual article data, and an administrator account with the user name admin and password admin.
+**生成虚拟数据：**
 
 ```
 $ python -m scripts.fake
 ```
 
-## Run
+除了一些文章和评论外，还创建一个管理员帐户，用户名为admin，密码为admin。
 
-run by default:
+**邮件服务：**
+
+如果需要配置邮件服务的话，可以将 `.env.example` 文件拷贝一份并保存为 `.env`，并根据你自己的邮件服务进行配置。
+
+## 运行
+
+内置服务器运行：
 
 ```
-$ python manage.py runserver
+$ python manage.py runserver 127.0.0.1:8000
 ```
 
-Enter the address http://127.0.0.1:8000 in the browser to access it.
+在浏览器中输入地址 http://127.0.0.1:8000 来访问它。
 
--- scripts/fake.py
+++ scripts/fake.py
@@ -2,21 +2,72 @@ import os
 import sys
 
 import django
+
 import faker
-from django.utils import timezone
+from random import randint
 
 # 将项目根目录添加到 Python 的模块搜索路径中
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 
+
+def genaerate_data():
+
+    # 消除多余的属性访问，提升运行效率
+    article_create = ArticlePost.objects.create
+    comment_create = Comment.objects.create
+
+    print('Some articles and comments will be created, which may take a little time.')
+    locales = ['', 'zh_CN']
+    luanguages = ['English', 'Chinese']
+    for locale, language in zip(locales, luanguages):
+        print("Create articles and comments in {}...".format(language))
+        fake = faker.Faker(locale=locale)  # 创建 Fake对象，设置语言
+        # 消除属性访问
+        fake_date_time_between = fake.date_time_between
+        fake_paragraphs = fake.paragraphs
+        fake_sentence = fake.sentence
+        del fake
+
+        i = None
+        for i in range(1, 101):
+            # 返回 2 个指定日期间的随机日期。三个参数分别是起始日期，终止日期和时区。
+            created_time = fake_date_time_between(
+                start_date='-1y',  # 1 年前
+                end_date="now",  # 终止日期为当下
+                tzinfo=get_current_timezone())
+            post = article_create(
+                title=fake_sentence().rstrip('.'),
+                # 用 2 个换行符连起来是为了符合 Markdown 语法，Markdown 中只有 2 个换行符分隔的文本才会被解析为段落
+                body='\n\n'.join(fake_paragraphs(randint(1, 20))),  # 10 个段落文本
+                created=created_time,
+                author=user,
+                total_views=randint(10, i + 10),
+            )
+            post.save()
+            # 为当前文件添加一些评论
+            j = None
+            for j in range(1, 6):
+                comment = comment_create(article=post,
+                                         user=user,
+                                         body='\n\n'.join(fake_paragraphs(j)),
+                                         created=created_time)
+                comment.save()
+
+
 if __name__ == '__main__':
+    from time import perf_counter
+    start = perf_counter()
+
     # 加载 django 配置项
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.development")
     django.setup()  # 启动 django
 
-    from article.models import ArticlePost
+    from django.utils.timezone import get_current_timezone
     from django.contrib.auth.models import User
-    from random import randint
+
+    from article.models import ArticlePost
+    from comment.models import Comment
 
     print('clear database.')
     ArticlePost.objects.all().delete()
@@ -25,32 +76,8 @@ if __name__ == '__main__':
     print('create a superuser.')
     user = User.objects.create_superuser('admin', '', 'admin')
 
-    print('create some faked posts published within the past year.')
-    fake = faker.Faker()  # English
-    for _ in range(100):
-        created = fake.date_time_between(  # 返回 2 个指定日期间的随机日期。三个参数分别是起始日期，终止日期和时区。
-            start_date='-1y',  # 1 年前
-            end_date="now",  # 终止日期为当下
-            tzinfo=timezone.get_current_timezone())
-        post = ArticlePost.objects.create(
-            title=fake.sentence().rstrip('.'),
-            # 用 2 个换行符连起来是为了符合 Markdown 语法，Markdown 中只有 2 个换行符分隔的文本才会被解析为段落
-            body='\n\n'.join(fake.paragraphs(10)),  # 10 个段落文本
-            created=created,
-            author=user,
-            total_views=randint(1, 100),
-        )
-        post.save()
-
-    fake = faker.Faker('zh_CN')
-    for _ in range(100):  # Chinese
-        created = fake.date_time_between(start_date='-1y', end_date="now", tzinfo=timezone.get_current_timezone())
-        post = ArticlePost.objects.create(
-            title=fake.sentence().rstrip('.'),
-            body='\n\n'.join(fake.paragraphs(10)),
-            author=user,
-            total_views=randint(1, 100),
-        )
-        post.save()
+    # 生成虚拟文章和评论
+    genaerate_data()
 
     print('done!')
+    print("It ran for {} seconds.".format(perf_counter() - start))
