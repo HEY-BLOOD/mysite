6ee6e1c1d9c67961e160d384bd2956118e659240
blood <blood@example.com>
2020-12-02 18:16:36 +0800

    更新 scripts.fake脚本（添加栏目和标签虚拟数据）

--- scripts/fake.py
+++ scripts/fake.py
@@ -15,20 +15,30 @@ def genaerate_data():
 
     # 消除多余的属性访问，提升运行效率
     article_create = ArticlePost.objects.create
+    column_create = ArticleColumn.objects.create
     comment_create = Comment.objects.create
 
     print('Some articles and comments will be created, which may take a little time.')
     locales = ['', 'zh_CN']
     luanguages = ['English', 'Chinese']
     for locale, language in zip(locales, luanguages):
-        print("Create articles and comments in {}...".format(language))
+        print("Now, Generate some virtual data in {} ...".format(language))
         fake = faker.Faker(locale=locale)  # 创建 Fake对象，设置语言
         # 消除属性访问
         fake_date_time_between = fake.date_time_between
         fake_paragraphs = fake.paragraphs
         fake_sentence = fake.sentence
+        fake_word = fake.word
         del fake
 
+        print("Create some article columns.")
+        column_list = []
+        for _ in range(4):
+            c = column_create(title=fake_word())
+            column_list.append(c)
+            c.save()
+
+        print("Create some tagged articles and comments ...")
         i = None
         for i in range(1, 101):
             # 返回 2 个指定日期间的随机日期。三个参数分别是起始日期，终止日期和时区。
@@ -43,15 +53,17 @@ def genaerate_data():
                 created=created_time,
                 author=user,
                 total_views=randint(10, i + 10),
+                column=column_list[i % len(column_list)],
+                # tags=[fake_word() for _ in range(3)],
             )
+            # 添加文章标签
+            tags = [fake_word() for _ in range(3)]
+            post.tags.add(tags[0], tags[1], tags[2])
             post.save()
-            # 为当前文件添加一些评论
+            # 为当前文章添加一些评论
             j = None
             for j in range(1, 6):
-                comment = comment_create(article=post,
-                                         user=user,
-                                         body='\n\n'.join(fake_paragraphs(j)),
-                                         created=created_time)
+                comment = comment_create(article=post, user=user, body='\n\n'.join(fake_paragraphs(j)), created=created_time)
                 comment.save()
 
 
@@ -59,19 +71,21 @@ if __name__ == '__main__':
     from time import perf_counter
     start = perf_counter()
 
-    # 加载 django 配置项
+    # 加载 django 配置项，使用开发环境配置
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.development")
     django.setup()  # 启动 django
 
     from django.utils.timezone import get_current_timezone
     from django.contrib.auth.models import User
-
     from article.models import ArticlePost
+    from article.models import ArticleColumn
     from comment.models import Comment
 
     print('clear database.')
     ArticlePost.objects.all().delete()
+    ArticleColumn.objects.all().delete()
     User.objects.all().delete()
+    Comment.objects.all().delete()
 
     print('create a superuser.')
     user = User.objects.create_superuser('admin', '', 'admin')
